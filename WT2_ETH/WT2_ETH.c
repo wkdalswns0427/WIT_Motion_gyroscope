87df4a3881621c060922492343112afa8fc002d8
MinJun <68832065+wkdalswns0427@users.noreply.github.com>
2022-04-11 10:44:11 +0900

    Update and rename WT2_ETH.ino to WT2_ETH.c

--- WT2_ETH/WT2_ETH.c
+++ WT2_ETH/WT2_ETH.c
@@ -1,20 +1,23 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiServer.h>
-const char* ssid = "toysmyth24G";
-const char* password = "xhdlTmalTm12#$";
-const uint16_t port = 5000;//sample
-const char* host = "3.38.162.240"; // new RDS
-WiFiClient client;
-
 #include <HardwareSerial.h> //for rs485 comm
 #define RXD2 5
 #define TXD2 17
 HardwareSerial rs485(2); // rxtx mode 2 of 0,1,2
+#define device_mac "D86595012345"
+#define SENSOR_ACC1_X 600
+#define SENSOR_ACC1_Y 601
+#define SENSOR_ACC1_Z 602
+#define SENSOR_ANG1_X 603
+#define SENSOR_ANG1_Y 604
+#define SENSOR_ANG1_Z 605
+#define SENSOR_ANGVEL1_X 606
+#define SENSOR_ANGVEL1_Y 607
+#define SENSOR_ANGVEL1_Z 608
+#define ERRCNT_FAIL_THRESHOLD 10
+#define ERRCNT_SUCCESS_THRESHOLD 3
 
-/*
 #include <Arduino.h> //for ethernet
 #include <ETH.h>     //for ethernet
+#include <ArduinoJson.h>
 #define ETH_POWER_PIN   16
 #define ETH_MDC_PIN     23
 #define ETH_MDIO_PIN    18
@@ -24,7 +27,11 @@ HardwareSerial rs485(2); // rxtx mode 2 of 0,1,2
 static bool eth_connected = false;
 #include <HTTPClient.h>
 #include <string.h>
-*/
+StaticJsonDocument<1024> sensor;
+
+unsigned long long int uS_TO_S_FACTOR = 1000000ULL;
+unsigned long long int TIME_TO_SLEEP = 0;
+RTC_DATA_ATTR int bootCount = 0;
 
 #include <math.h>
 byte unlockMaster1[]={0x50, 0x06, 0x00, 0x69, 0xB5, 0x88, 0x22, 0xA1};
@@ -52,6 +59,7 @@ byte trashBuffer[150];
 
 byte prevBuffer[3][6];
 byte newBuffer[3][6];
+byte diffBuffer[3][6];
 /*
  **********************************************************************
  *           dev1[x] | dev1[y] | dev1[z] | dev2[x] | dev2[y] | dev2[z] |
@@ -65,13 +73,46 @@ byte newBuffer[3][6];
  ***********************************************************************
  */
 
-static byte accDiff[] = {0,0,0};
-static byte angDiff[] = {0,0,0};
-static byte angvelDiff[] = {0,0,0};
+static byte accDiff[6];
+static byte angDiff[6];
+static byte angvelDiff[6];
 
 int flag = 0;
 
-/*
+struct errcnt {
+    unsigned short success = 0;
+    unsigned short fail = 0;
+};
+struct errcnt err_api;
+struct errcnt err_wt901_1;
+struct errcnt err_wt901_2;
+
+
+
+void reboot()
+{
+    Serial.println("system reboot");
+    ESP.restart();
+}
+
+void errcnt_countup(struct errcnt* count, int fail)
+{
+    if (fail) {
+        count->success = 0;
+        count->fail++;
+        if (count->fail == ERRCNT_FAIL_THRESHOLD)
+
+            reboot();
+    } else {
+        count->success++;
+        if (count->success >= ERRCNT_SUCCESS_THRESHOLD) {
+            count->success--;
+            count->fail = 0;
+        }
+    }
+}
+
+
 void WiFiEvent(WiFiEvent_t event)
 {
 
@@ -109,7 +150,32 @@ void WiFiEvent(WiFiEvent_t event)
       break;
   }
 }
-*/
+
+
+void print_wakeup_reason() {
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break; //should always be this
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
+  }
+}
+
+void config_sleep_mode() {
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.print("Setup ESP32 to sleep for every ");
+  Serial.print(TIME_TO_SLEEP);
+  Serial.println(" Seconds");
+  print_wakeup_reason();
+}
+
 
 void sendCommand(byte command[8], int prt){
   byte data[10];
@@ -366,14 +432,15 @@ void printAngVel(byte rec[]){
 byte* calculateDiff(byte arr[], int device, int type){
   for(int i=3*(device-1); i<3*(device-1)+3; i++){
     if(type==0){
-        arr[i] = (abs(prevBuffer[type][i]/(32768/16)-newBuffer[type][i+3]/(32768/16)));
+        arr[i] = (abs(prevBuffer[type][i]/(32768/16)-newBuffer[type][i]/(32768/16)));
       }
     else if(type==1){
-        arr[i] = (abs(prevBuffer[type][i]/(32768/180)-newBuffer[type][i+3]/(32768/180)));
+        arr[i] = (abs(prevBuffer[type][i]/(32768/180)-newBuffer[type][i]/(32768/180)));
       }
     else if(type==2){
-        arr[i] = (abs(prevBuffer[type][i]/(32768/2000)-newBuffer[type][i+3]/(32768/2000)));
+        arr[i] = (abs(prevBuffer[type][i]/(32768/2000)-newBuffer[type][i]/(32768/2000)));
       }
+    diffBuffer[type][i]=arr[i];
     }
   if(device==1){
     Serial.print(arr[0]);Serial.print("   "); Serial.print(arr[1]);Serial.print("   "); Serial.println(arr[2]);
@@ -384,28 +451,123 @@ byte* calculateDiff(byte arr[], int device, int type){
   return arr;
   }
 
+void readSensor(byte buf[3][6]){
+  readAcceleration(1);
+  savebuffer(buf, 1, 0);
+  readAcceleration(2);
+  savebuffer(buf, 2, 0);
+  
+  readSensorAngle(1);
+  savebuffer(buf, 1, 1);
+  readSensorAngle(2);
+  savebuffer(buf, 2, 1);
+    
+  readAngularVelocity(1);
+  savebuffer(buf, 1, 2);
+  readAngularVelocity(2);
+  savebuffer(buf, 2, 2);
+  Serial.println();
+}
+
+void calcSensor(){
+  calculateDiff(accDiff, 1, 0);
+  calculateDiff(accDiff, 2, 0);
+  calculateDiff(angDiff, 1, 1);
+  calculateDiff(angDiff, 2, 1);
+  calculateDiff(angvelDiff, 1, 2);
+  calculateDiff(angvelDiff, 2, 2);
+}
+
+void clearBuffer(){
+  for(int i=0; i<3;i++){
+    for(int j=0; j<6;j++){
+      prevBuffer[i][j] = newBuffer[i][j];
+      }
+  }
+}
+void sensorPOST(){
+  sensor["mac"]= device_mac;
+  sensor["type"]="SENSOR";
+  sensor["data"][0]["sensortype"] = SENSOR_ACC1_X;
+  sensor["data"][0]["value"] = diffBuffer[0][0];
+//  sensor["data"][0]["time"] = time_us;
+  sensor["data"][1]["sensortype"] = SENSOR_ACC1_Y;
+  sensor["data"][1]["value"] = diffBuffer[0][1];
+//  sensor["data"][1]["time"] = time_us;
+  sensor["data"][2]["sensortype"] = SENSOR_ACC1_Z;
+  sensor["data"][2]["value"] = diffBuffer[0][2];
+//  sensor["data"][2]["time"] = time_us;
+  sensor["data"][3]["sensortype"] = SENSOR_ANG1_X;
+  sensor["data"][3]["value"] = diffBuffer[1][0];
+//  sensor["data"][3]["time"] = time_us;
+  sensor["data"][4]["sensortype"] = SENSOR_ANG1_Y;
+  sensor["data"][4]["value"] = diffBuffer[1][1];
+//  sensor["data"][4]["time"] = time_us;
+  sensor["data"][5]["sensortype"] = SENSOR_ANG1_Z;
+  sensor["data"][5]["value"] = diffBuffer[1][2];
+//  sensor["data"][5]["time"] = time_us;
+  sensor["data"][6]["sensortype"] = SENSOR_ANGVEL1_X;
+  sensor["data"][6]["value"] = diffBuffer[2][0];
+//  sensor["data"][6]["time"] = time_us;
+  sensor["data"][7]["sensortype"] = SENSOR_ANGVEL1_Y;
+  sensor["data"][7]["value"] = diffBuffer[2][1];
+//  sensor["data"][7]["time"] = time_us;
+  sensor["data"][8]["sensortype"] = SENSOR_ANGVEL1_Z;
+  sensor["data"][8]["value"] = diffBuffer[2][2];
+//  sensor["data"][8]["time"] = time_us;
+  }
+
+void postHTTP(){
+  HTTPClient http;
+  sensorPOST();
+  String requestBody;
+  serializeJson(sensor, requestBody);
+
+  http.begin("http://restapi.toysmythiot.com:8081/v1/sensor/insert");
+  http.addHeader("Content-Type", "application/json", "Content-Length", requestBody.length());
+
+  int httpResponseCode = http.POST(requestBody);
+  if (httpResponseCode > 0) {
+      errcnt_countup(&err_api, 0);
+      Serial.print("sensor data post result: ");
+      Serial.println(httpResponseCode);
+      Serial.println(http.getString());
+  } else {
+      errcnt_countup(&err_api, 1);
+      Serial.print("error with httpResponseCode: ");
+      Serial.println(httpResponseCode);
+  }
+
+  sensor.clear();
+  requestBody.clear();
+  http.end();
+
+}
+
+
 void setup() 
 { 
   Serial.begin(9600);
   rs485.begin(9600, SERIAL_8N1, RXD2, TXD2);
   rs485.flush();
-  // wifi
-  /*
-  WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.println("...");
-    }
-  Serial.print("WiFi connected with IP : ");
-  Serial.println(WiFi.localIP());
-  */
-  
+
   // ethernet
-  /*
+  int cnt = 0;
   WiFi.onEvent(WiFiEvent);
   ETH.begin(ETH_ADDR, ETH_POWER_PIN, ETH_MDC_PIN, ETH_MDIO_PIN, ETH_TYPE, ETH_CLK_MODE);
-  delay(10000);
-  */
+  do {
+    if (++cnt == 10) {
+      TIME_TO_SLEEP = (50 * 60);
+      config_sleep_mode();
+      Serial.println("Can not connect ethernet -> ESP sleep now");
+      Serial.flush();
+      esp_deep_sleep_start();
+    }
+    Serial.println("Waiting for ethernet connection");
+    delay(10000);
+  } while (!eth_connected);
+
+  
   
   Serial.println("--------------- Serial Initiated ---------------");
   calibrateAcc(1);
@@ -427,79 +589,16 @@ void setup()
 
 void loop() 
 { 
-  /*
-  int trial = 1;
-  if(!client.connect(host, port)){
-    Serial.println("Connection to Host Failed");
-    delay(1000);
-    trial++;
-    return;
-    }
-  */
   rs485.flush();
   if(++flag==1){
     Serial.println("WT901C485 read");
-    readAcceleration(1);
-    savebuffer(prevBuffer, 1, 0);
-    readAcceleration(2);
-    savebuffer(prevBuffer, 2, 0);
-    
-    readSensorAngle(1);
-    savebuffer(prevBuffer, 1, 1);
-    readSensorAngle(2);
-    savebuffer(prevBuffer, 2, 1);
-    
-    readAngularVelocity(1);
-    savebuffer(prevBuffer, 1, 2);
-    readAngularVelocity(2);
-    savebuffer(prevBuffer, 2, 2);
+    readSensor(prevBuffer);
     delay(500);
-    for(int i=0; i<3;i++){
-      for(int j=0; j<6;j++){
-        Serial.print(prevBuffer[i][j]);Serial.print("   ");
-        }
-      Serial.println();
-      }
-    }
-  
-  readAcceleration(1);
-  savebuffer(newBuffer, 1, 0);
-  readAcceleration(2);
-  savebuffer(newBuffer, 2, 0);
-  
-  readSensorAngle(1);
-  savebuffer(newBuffer, 1, 1);
-  readSensorAngle(2);
-  savebuffer(newBuffer, 2, 1);
-    
-  readAngularVelocity(1);
-  savebuffer(newBuffer, 1, 2);
-  readAngularVelocity(2);
-  savebuffer(newBuffer, 2, 2);
-
-  Serial.println();
-  calculateDiff(accDiff, 1, 0);
-  calculateDiff(accDiff, 2, 0);
-  calculateDiff(angDiff, 1, 1);
-  calculateDiff(angDiff, 2, 1);
-  calculateDiff(angvelDiff, 1, 2);
-  calculateDiff(angvelDiff, 2, 2);
-  for(int i=0; i<3;i++){
-      for(int j=0; j<6;j++){
-        Serial.print(prevBuffer[i][j]);Serial.print("   ");
-        }
-      Serial.println();
-      }
-  for(int i=0; i<3;i++){
-      for(int j=0; j<6;j++){
-        Serial.print(newBuffer[i][j]);Serial.print("   ");
-        }
-      Serial.println();
-      }
-  for(int i=0; i<3;i++){
-    for(int j=0; j<6;j++){
-      prevBuffer[i][j] = newBuffer[i][j];
-      }
   }
+  
+  readSensor(newBuffer);
+  calcSensor();
+  postHTTP();
+  clearBuffer();
   delay(500);
 }
